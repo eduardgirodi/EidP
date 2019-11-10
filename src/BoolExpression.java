b71a39044b7965a1e6282777fb411a9166af243d
Eduard Girodi <eduard@girodi.de>
2019-11-10 17:10:36 +0100

    Create BoolExpression.java
    Add solution for exercise 3 task 6

---
+++ src/BoolExpression.java
@@ -0,0 +1,9 @@
+public class BoolExpression {
+	public static void main(String[] args) {
+		boolean wert1 = false;
+		boolean wert2 = true;
+		System.out.println(!wert1&(wert1|wert2));
+		wert1 = true;
+		System.out.println(!wert1&(wert1|wert2));
+	}
+}
