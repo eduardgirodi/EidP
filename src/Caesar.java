be09d6050d7bec85573da834cb2f0fb35f2a7d3f
Eduard Girodi <eduard@girodi.de>
2019-11-12 19:33:25 +0100

    Update Caesar.java
    Refactor code in src/Caesar.java

--- src/Caesar.java
+++ src/Caesar.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 
 public class Caesar {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int k = s.nextInt(); // Key
         k = k % 26;
         String b = s.next().toUpperCase();
         s.close();
-
         String ergebnis = "";
-
         for (int i = 0; i < b.length(); i++) {
             char c = b.charAt(i);
             int character = c + k;
@@ -20,6 +18,5 @@ public class Caesar {
             ergebnis += String.valueOf((char) character);
         }
         System.out.println(ergebnis);
-
     }
 }
