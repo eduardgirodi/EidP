033c0f98d4c44097ad715bc4b993700f54b2743a
Eduard Girodi <eduard@girodi.de>
2019-11-24 22:17:21 +0100

    Update src/Vertauschen.java
    Modify code and empty main() Method

--- src/Vertauschen.java
+++ src/Vertauschen.java
@@ -3,13 +3,11 @@ public class Vertauschen {
     public static String[] vertauscheInhalt(String[] pArray) {
         String[] ergebnis = new String[pArray.length];
         for(int i=0;i<pArray.length;i++) {
-            String temp = pArray[i];
-            ergebnis[pArray.length-1-i] = temp;
+            ergebnis[pArray.length-1-i] = new String(pArray[i]);
         }
         return ergebnis;
     }
 
-    
     public static String[] vertauscheReferenz(String[] pArray) {
         String[] ergebnis = new String[pArray.length];
         for(int i=0;i<pArray.length;i++) {
@@ -28,21 +26,13 @@ public class Vertauschen {
     }
 
     public static boolean testReferenzGleich(String[] arg1, String[] arg2) {
-        if(arg1==arg2) {
-            return false;
+        for(int i=0;i<arg1.length;i++) {
+            if(!(arg1[i]==arg2[i])) {
+                return false;
+            }
         }
         return true;
     }
 
-    public static void main(String[] args) {
-        String[] inhalt = vertauscheInhalt(args);
-        String[] referenz = vertauscheReferenz(args);
-
-        if(!testInhaltGleich(inhalt, referenz)) {
-            System.out.println("Fehler: inhalte stimmen nicht ueberein!");
-        }
-        if(testReferenzGleich(inhalt, referenz)) {
-            System.out.println("Fehler: Referenzen muessen unterschiedlich sein!");
-        }
-    }
+    public static void main(String[] args) { }
 }
