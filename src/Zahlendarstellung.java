7ab853d8d48d4e62f65cd0b4bb1a35fd441c6265
Eduard Girodi <eduard@girodi.de>
2019-11-25 11:19:45 +0100

    Create src/Zahlendarstellung.java
    Add solution for exercise 9 task 2

---
+++ src/Zahlendarstellung.java
@@ -0,0 +1,30 @@
+public class Zahlendarstellung {
+
+    public static int ermittleLaenge(int x, int b) {
+        int count = 0;
+        while(x >= 1) {
+            count++;
+            x = x/b;
+        }
+        return count;
+    }
+
+    public static int[] ermittleZiffern(int x, int b) {
+        if(x==0) {
+            int[] ergebnis = {0};
+            return ergebnis;
+        }
+
+        int[] ergebnis = new int[ermittleLaenge(x,b)];
+
+        for(int i=0; x>=1; i++) {
+            ergebnis[i] = x%b;
+            x = x/b;
+        }
+        return ergebnis;
+    }
+
+    public static void main(String[] args) {
+        }
+    }
+ }
