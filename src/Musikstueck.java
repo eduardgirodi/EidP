17233501bc32f6b640227202207741a509548397
Eduard Girodi <eduard@girodi.de>
2019-12-15 19:14:26 +0100

    Update src/Musikstueck.java
    Refactor code

--- src/Musikstueck.java
+++ src/Musikstueck.java
@@ -1,56 +1,19 @@
-public class Musikstueck {
+//package UA10;
 
-    String[] interpreten = new String[0];
-    String titel;
-    int[] toene;
-    int anzahlEinzelBewertungen;
-    int gesamtBewertung = 0;
+public class Musikstueck
+{
+    private String[] interpreten;
+    private String titel;
+    private int[] toene;
+    private int anzahlEinzelBewertungen;
+    private int gesamtBewertung;
 
-    public Musikstueck(String[] interpreten, String titel, int[] toene) {
-        this.interpreten = interpreten;
-        this.titel = titel;
-        this.toene = toene;
-    }
-
-    public String printToene() {
-        String ergebnis = "";
-        for(int i=0; i<toene.length; i++) {
-            ergebnis += toene[i] +" ";
-        }
-        return ergebnis;
-    }
-
-    public String printInterpreten() {
-        String ergebnis = "";
-        for(int i=0; i<interpreten.length; i++) {
-            ergebnis += interpreten[i] +", ";
-        }
-        return ergebnis;
-    }
-
-    public void abspielen() {
-        System.out.println(this.printToene());
-    }
-
-    public void neueBewertung(int bewertung) {
-        anzahlEinzelBewertungen++;
-        gesamtBewertung = (gesamtBewertung*anzahlEinzelBewertungen)+bewertung;
-        gesamtBewertung = Math.round(gesamtBewertung/anzahlEinzelBewertungen);
-    }
-
-    public int getGesamtbewertung() {
-        return gesamtBewertung;
-    }
-
-    public String toString() {
-        return "Interpreten: " +this.printInterpreten() +"Titel: " +titel +", " +"Toene: " +this.printToene() +", " +"Anz. Bewertungen: " +anzahlEinzelBewertungen +", "+"Gesamtbewertung: " +getGesamtbewertung();
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         String[] interpreten1 = {"Wham"};
-        String[] interpreten2 = {"Koelner Domspatzen", "Heino"};
-        int[] toene1 = {1, 2, 3, 4, 5, 6};
-        int[] toene2 = {6, 5, 4, 3, 2, 1};
+        String[] interpreten2 = {"Koelner Dompspatzen", "Heino"};
+        int[] toene1 = {1,2,3,4,5,6};
+        int[] toene2 = {6,5,4,3,2,1};
 
         Musikstueck m1 = new Musikstueck(interpreten1, "Last Christmas", toene1);
         Musikstueck m2 = new Musikstueck(interpreten2, "O Tannenbaum", toene2);
@@ -61,5 +24,53 @@ public class Musikstueck {
         m1.neueBewertung(3);
         System.out.println(m1);
         System.out.println(m2);
+
+
+    }
+
+    public Musikstueck(String[] performers, String title, int[] tunes){
+        this.interpreten = performers;
+        this.titel = title;
+        this.toene = tunes;
+        this.anzahlEinzelBewertungen = 0;
+        this.gesamtBewertung = 0;
+    }
+
+    public void abspielen(){
+        for (int tune : this.toene)
+        {
+            System.out.print(tune + " ");
+        }
+        System.out.println();
+    }
+
+    public void neueBewertung(int bewertung){
+        this.gesamtBewertung += bewertung;
+        this.anzahlEinzelBewertungen++;
+    }
+
+    public int getGesamtbewertung(){
+        if(this.anzahlEinzelBewertungen == 0)return 0;
+        int totalRating = (int) Math.round(((double)this.gesamtBewertung/(double)this.anzahlEinzelBewertungen));
+        if(totalRating >5){totalRating = 5;}
+        else if(totalRating < 1){totalRating = 1;}
+        return totalRating;
+    }
+
+    public String toString(){
+        int totalRating = this.getGesamtbewertung();
+        String output = "Interpreten: ";
+        for (String performer : this.interpreten)
+        {
+            output += performer +", ";
+        }
+        output += "Titel: " + this.titel + ", Toene: ";
+        for (int tune : this.toene)
+        {
+            output += tune + " ";
+        }
+        output += ", Anz. Bewertung: " + this.anzahlEinzelBewertungen + ", Gesamtbewertung: " + totalRating;
+        return output;
     }
 }
+
